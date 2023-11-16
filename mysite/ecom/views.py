13450fc8de412845bb59fba0e63f7dbce742a7c0
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-11-16 16:45:58 +0530

    Commit:6

--- mysite/ecom/views.py
+++ mysite/ecom/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is an index view')
+    return HttpResponse('<h1 style = "color : Red ">This is an index view</h1>')
+
+def detail(request):
+    return HttpResponse('<h1 style = "color : Orange">This is a details page</h1>')
