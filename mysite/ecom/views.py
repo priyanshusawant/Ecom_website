b8c46c7867be5d0232b21cdd15d080777f12e3e5
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-11-17 12:05:59 +0530

    Commit:10-14

--- mysite/ecom/views.py
+++ mysite/ecom/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ecom.models import Item
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1 style = "color : Red ">This is an index view</h1>')
+    itemlist = Item.objects.all()
+
+    context = {
+        'itemlist':itemlist
+    }
+
+    return render(request, 'ecom/index.html', context)
 
 def detail(request):
     return HttpResponse('<h1 style = "color : Orange">This is a details page</h1>')
