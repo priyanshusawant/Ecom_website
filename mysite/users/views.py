0f88a8c244aae0db3bb8a2a83e54b8d4a00d6f22
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-11-28 13:00:46 +0530

    Commit

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from users.forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request,
+                'Welcome {}, your account has been successfully created'.format(username)
+            )
+            form.save()
+            return redirect('ecom:index')
+        
+    else:
+
+        form = RegisterForm()
+        
+        context = {
+            'form':form
+        }
+
+    return render(request, 'users/register.html', context)
