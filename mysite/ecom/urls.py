9e4b8f339f2dc4f5ebb658483677b2613f5e3976
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-12-15 20:23:28 +0530

    Commit

--- mysite/ecom/urls.py
+++ mysite/ecom/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
 
 # function based index view
 # -------------------------------------------------------------------------------------------------------------------------------------
-    # path('home/', views.index, name='index'),
+    path('home/', views.index, name='index'),
 
 # class based index view
 # -------------------------------------------------------------------------------------------------------------------------------------
-    path('home/', views.IndexClassView.as_view(), name='index'),
+    # path('home/', views.IndexClassView.as_view(), name='index'),
 
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
